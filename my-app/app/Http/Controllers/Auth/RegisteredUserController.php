17ac23ac7e6b066e91a5d4c500eba534e2676785
yassinmftah <yassinmaftah06@gmail.com>
2026-02-01 19:06:09 +0100

    add phone and role and insert into database

--- my-app/app/Http/Controllers/Auth/RegisteredUserController.php
+++ my-app/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -29,16 +29,20 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
+        // dd($request->all());
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'phone' => ['required', 'string', 'max:20'],
+            'role' => ['required', 'string', 'in:passenger,admin,driver'],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'role' => $request->role,
         ]);
 
         event(new Registered($user));
