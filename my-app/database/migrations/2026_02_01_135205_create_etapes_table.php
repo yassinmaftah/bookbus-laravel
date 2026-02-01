aed47babaef167ac5318681a950e5a93491e5991
yassinmftah <yassinmaftah06@gmail.com>
2026-02-01 15:38:11 +0100

    create table etapes in database

--- my-app/database/migrations/2026_02_01_135205_create_etapes_table.php
+++ my-app/database/migrations/2026_02_01_135205_create_etapes_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('etapes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('route_id')->constrained('routes')->onDelete('cascade');
+            $table->foreignId('gare_id')->constrained('gares')->onDelete('cascade');
+            $table->integer('ordre');
+            $table->time('heure_passage')->nullable();
             $table->timestamps();
         });
     }
