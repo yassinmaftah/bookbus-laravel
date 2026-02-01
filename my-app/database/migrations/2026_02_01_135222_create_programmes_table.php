9516106a9c82b78995fa69f52d0925254d0d6d7e
yassinmftah <yassinmaftah06@gmail.com>
2026-02-01 15:47:16 +0100

    create table programmes in database

--- my-app/database/migrations/2026_02_01_135222_create_programmes_table.php
+++ my-app/database/migrations/2026_02_01_135222_create_programmes_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('programmes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('bus_id')->constrained('buses');
+            $table->foreignId('route_id')->constrained('routes');
+            $table->date('jour_depart');
+            $table->time('heure_depart');
+            $table->time('heure_arrivee');
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
