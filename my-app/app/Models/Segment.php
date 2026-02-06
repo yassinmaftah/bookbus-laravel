8755615e9b79d2507e2309bc813e0eee1e310fca
yassinmftah <yassinmaftah06@gmail.com>
2026-02-06 15:35:02 +0100

    Model Segment with methods that return point depart and point arrivee and bus

--- my-app/app/Models/Segment.php
+++ my-app/app/Models/Segment.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Segment extends Model
 {
-    //
+    public function programme()
+    {
+        return $this->belongsTo(Programme::class);
+    }
+    public function bus()
+    {
+        return $this->belongsTo(Bus::class);
+    }
+
+    public function etapeDepart()
+    {
+        return $this->belongsTo(Etape::class, 'etape_depart_id');
+    }
+
+    public function etapeArrivee()
+    {
+        return $this->belongsTo(Etape::class, 'etape_arrivee_id');
+    }
 }
+
