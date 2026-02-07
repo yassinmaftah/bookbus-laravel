cf3e3ccd9b916147c5a5e66bcbab10b181122a56
yassinmftah <yassinmaftah06@gmail.com>
2026-02-07 16:32:14 +0100

    add Gare model with ville and etape relations

--- my-app/app/Models/Gare.php
+++ my-app/app/Models/Gare.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Gare extends Model
 {
-    //
+    public function ville(){return $this->belongsTo(Ville::class);}
+    public function etapes(){return $this->hasMany(Etape::class);}
 }
