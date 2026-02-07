a6d187acb2beb987de6351a59813056bb3a37508
yassinmftah <yassinmaftah06@gmail.com>
2026-02-07 16:42:43 +0100

    create Route model linking to Etapes, Programmes and Segments

--- my-app/app/Models/Route.php
+++ my-app/app/Models/Route.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Route extends Model
 {
-    //
+    public function segments()
+    {
+        return $this->hasManyThrough(Segment::class,Etape::class,
+                                    'route_id','etape_depart_id');
+    }
+    public function programmes(){return $this->hasMany(Programme::class);}
+    public function etapes(){return $this->hasMany(Etape::class);}
 }
